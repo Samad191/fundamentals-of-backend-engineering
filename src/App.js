3816e4b99d9a07055b818614d91cc2c67eb3e365
Samad191 <samad13354@gmail.com>
2024-10-17 09:36:40 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let sse;
+ 
+   
+
+  const handleClick = () => {
+    console.log('click')
+ 
+    sse = new EventSource('http://localhost:8080/stream')
+    sse.onmessage = (event) => {
+      console.log('message', event?.data)
+    }
+    console.log('outside message event')
+
+  }
+
+  const handleMultipleStreamEvents = () => {
+    sse = new EventSource('http://localhost:8080/multipleStreams')
+    sse.addEventListener('event1', (event) => {
+      console.log('event1', event.data);
+    });
+  
+    sse.addEventListener('event2', (event) => {
+      console.log('event2', event.data);
+    });
+
+    sse.onopen = () => {
+      console.log('Connection opened');
+    };
+  
+    sse.onerror = (error) => {
+      console.error('Error:', error);
+    };
+    
+  }
+
+  const handleClose = () => {
+    console.log('close')
+    sse.close()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Server side events</p>
+      <button onClick={handleClick} >Start</button>
+      <button onClick={handleClose} >Close</button>
+      <button onClick={handleMultipleStreamEvents} >Multiple Streams</button>
       </header>
     </div>
   );
